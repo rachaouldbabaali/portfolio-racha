7502d043a1fb9df8f307e2e0b7fa0604f37636e5
Racha OULDBABAALI <rachaould@gmail.com>
2023-11-05 16:10:27 +0100

    Fix menuoverlay

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
+
+  const scrollToHash = (hash) => {
+    const el = document.querySelector(hash);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
-        <li key={index}>
-          <NavLink href={link.path} title={link.title} />
+        <li key={index} onClick={() => scrollToHash(link.path)} className="cursor-pointer block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white" >
+          {link.title}
         </li>
       ))}
     </ul>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef  } from "react";
-import { Link } from "react-scroll/modules";
+import Link from "next/link";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";

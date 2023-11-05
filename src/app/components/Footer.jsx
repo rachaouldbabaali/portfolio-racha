e37d56a2f56246c3be1c71026d50c9d90a737a0e
Racha OULDBABAALI <rachaould@gmail.com>
2023-11-05 15:30:12 +0100

    Fix navbar responsivness

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-center">
+      <div className="container p-6 flex justify-center items-center">
         <p>
            Made with <span className="text-red-600">❤</span> by{" Racha Ould Babaali "}
         </p>
--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "react-scroll";
 
 const NavLink = ({ href, title }) => {
   return (
